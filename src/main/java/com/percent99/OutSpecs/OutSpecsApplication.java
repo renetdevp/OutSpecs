201bb4f143cdfd79f5bb8ff104ebcd875c15c4d1
renetdevp <renetdevp@gmail.com>
2025-07-31 18:04:21 +0900

    Initialize project
    Initialize Spring boot project

---
+++ src/main/java/com/percent99/OutSpecs/OutSpecsApplication.java
@@ -0,0 +1,13 @@
+package com.percent99.OutSpecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutSpecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutSpecsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/percent99/OutSpecs/OutSpecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.percent99.OutSpecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutSpecsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
