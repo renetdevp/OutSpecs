f13944c7830991c91293d8df5032a7e5ecd94eb1
Hasegos <csw020106@naver.com>
2025-08-01 15:08:33 +0900

    ADD : userRoleType Enum 추가
    사용자 상태에 따른 Enum 추가

---
+++ src/main/java/com/percent99/OutSpecs/entity/UserRoleType.java
@@ -0,0 +1,14 @@
+package com.percent99.OutSpecs.entity;
+
+/**
+ * user 상태에 따른 Enum <br><br>
+ * 
+ * ENTUSER : 기업회원 <br>
+ * USER : 일반회원 <br>
+ * ADMIN : 관리자
+ */
+public enum UserRoleType {
+    ENTUSER,
+    USER,
+    ADMIN
+}
