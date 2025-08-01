06a91bcda4f24173ac4803dd7a66a9638c9af806
renetdevp <renetdevp@gmail.com>
2025-08-01 15:07:12 +0900

    ADD: Define chatRoomUser entity
    Define chatRoomUser entity
    Field: chatRoom, user, joinedAt

---
+++ src/main/java/com/percent99/OutSpecs/entity/ChatRoomUser.java
@@ -0,0 +1,29 @@
+package com.percent99.OutSpecs.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * 어느 사용자가 어느 채팅방에 참여하고 있는지를 저장하기 위한 users와 chat_rooms의 중간 테이블.
+ * Field: chatRoom, user, joinedAt.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "chat_room_user")
+public class ChatRoomUser {
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "chat_room_id")
+  private ChatRoom chatRoom;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @Column(name = "joined_at")
+  private LocalDateTime joinedAt;
+}
