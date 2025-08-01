85cfc6925287334b88dd073fb90ffe627a96111e
renetdevp <renetdevp@gmail.com>
2025-08-01 15:29:58 +0900

    FIX: Fix chatMessage entity
    Add @Entity annotation to class ChatMessage
    Add column length to content column

--- src/main/java/com/percent99/OutSpecs/entity/ChatMessage.java
+++ src/main/java/com/percent99/OutSpecs/entity/ChatMessage.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 @Table(name = "chat_messages")
 public class ChatMessage {
   @Id
@@ -28,7 +29,7 @@ public class ChatMessage {
   @JoinColumn(name = "sender_id", nullable = false)
   private User sender;
 
-  @Column(name = "content", nullable = false)
+  @Column(name = "content", nullable = false, length = 255)
   private String content;
 
   @Column(name = "created_at", nullable = false)
