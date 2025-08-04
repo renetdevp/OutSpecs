c6233e4ecb3aa0bef2c35e12c158fd619675e315
renetdevp <renetdevp@gmail.com>
2025-08-04 16:18:31 +0900

    ADD: Add ChatRoom service
    Add ChatRoom repository
    Add ChatRoom service

---
+++ src/main/java/com/percent99/OutSpecs/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package com.percent99.OutSpecs.repository;
+
+import com.percent99.OutSpecs.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+}
