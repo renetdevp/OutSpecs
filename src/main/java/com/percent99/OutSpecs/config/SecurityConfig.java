3d6aff8c60a512bb0e123a8c39da034f1be7ba6e
Hasegos <csw020106@naver.com>
2025-08-05 18:03:36 +0900

    ADD : SecurityConfig 보안설정 추가
    OAuth2 , form 로그인 할경우 권한체크, 로그아웃, csrf 비활성화 추가

---
+++ src/main/java/com/percent99/OutSpecs/config/SecurityConfig.java
@@ -0,0 +1,104 @@
+package com.percent99.OutSpecs.config;
+
+
+import com.percent99.OutSpecs.security.CustomOAuth2UserService;
+import com.percent99.OutSpecs.security.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 애플리케이션의 보안 설정을 담당하는 Configuration 클래스
+ *
+ * <p>
+ *     이 클래스는 보안 설정을 담당합니다.
+ * </p>
+ * <ul>
+ *  <li>CSRF 보호 비활성화</li>
+ *  <li>정적 리소스 및 공개 경로에 대한 접근 허용</li>
+ *  <li>폼 로그인 및 OAuth2 로그인 설정</li>
+ *  <li>로그인 성공/실패 핸들러 설정</li>
+ *  <li>로그아웃 시 쿠키 삭제 및 리다이렉트 설정</li>
+ * </ul>
+ *
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomUserDetailService customUserDetailService;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider p = new DaoAuthenticationProvider();
+        p.setUserDetailsService(customUserDetailService);
+        p.setPasswordEncoder(passwordEncoder());
+        return p;
+    }
+
+    /**
+     * HTTP 요청에 대한 보안 설정 구성, 빈을 생성하여 반환한다.
+     *
+     * @param http HttpSecurity 객체
+     * @return 설정이 적용된 SecurityFilterChain 객체
+     * @throws Exception 보안 설정중 예외 발생시 발생
+     */
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/scripts/**").permitAll()
+                        .requestMatchers("/error","/").permitAll()
+                        .requestMatchers("/login", "/logout").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/users/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/", true)
+                        .failureHandler((request, response, exception) -> {
+                            String errorMsg = URLEncoder.encode(exception.getMessage(), StandardCharsets.UTF_8);
+                            response.sendRedirect("/users/login?error=" + errorMsg);
+                        })
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .deleteCookies("JSESSIONID")
+                )
+                .oauth2Login(oAuth2 -> oAuth2
+                        .loginPage("/users/login")
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                        .defaultSuccessUrl("/", true)
+                        .failureHandler((request, response, exception) -> {
+                            String errorMsg = URLEncoder.encode(exception.getMessage(), StandardCharsets.UTF_8);
+                            response.sendRedirect("/users/login?error=" + errorMsg);
+                        })
+                );
+
+        return http.build();
+    }
+}
