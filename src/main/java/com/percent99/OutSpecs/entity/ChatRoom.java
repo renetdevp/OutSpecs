2fbcb63f13b07178d54b073af3f08373cca15e60
renetdevp <renetdevp@gmail.com>
2025-08-05 16:06:30 +0900

    ADD: Add user1/user2 field into ChatRoom entity
    Add user1/user field into ChatRoom entity

--- src/main/java/com/percent99/OutSpecs/entity/ChatRoom.java
+++ src/main/java/com/percent99/OutSpecs/entity/ChatRoom.java
@@ -20,6 +20,14 @@ public class ChatRoom {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user1_id")
+    private User user1;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user2_id")
+    private User user2;
+
     @Column(name = "is_chatbot", nullable = false)
     private Boolean isChatbot;
 
