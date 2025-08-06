11e471ffd19486a7eca29b2f00150b85e2270969
renetdevp <renetdevp@gmail.com>
2025-08-06 21:42:39 +0900

    FIX: Replace result of AlanService.sendRequest() from HttpResponseString to parsed String value
    AlanService.sendRequest()는 기존에 API 호출 결과를 그대로 String 타입으로 반환했지만, 기능 수정 후 jackson으로 API 호출 결과를 파싱한 후 필요한 부분만 뽑아 반환

--- src/main/java/com/percent99/OutSpecs/service/AlanService.java
+++ src/main/java/com/percent99/OutSpecs/service/AlanService.java
@@ -1,7 +1,11 @@
 package com.percent99.OutSpecs.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.percent99.OutSpecs.exception.HttpResponseProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -10,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class AlanService {
   private final RestTemplate restTemplate;
+  private final ObjectMapper objectMapper;
 
   @Value("${alan.BASE_URL}")
   private String baseUrl;
@@ -17,14 +22,20 @@ public class AlanService {
   @Value("${alan.CLIENT_ID}")
   private String alanClientId;
 
-  private String sendRequest(String content){
+  private String sendRequest(String content) {
     String url = UriComponentsBuilder.fromUriString(baseUrl)
             .queryParam("content", content)
             .queryParam("client_id", alanClientId)
             .build()
             .toUriString();
 
-    return restTemplate.getForObject(url, String.class);
+    ResponseEntity<String> res = restTemplate.getForEntity(url, String.class);
+
+    try {
+      return objectMapper.readTree(res.getBody()).get("content").asText();
+    } catch (JsonProcessingException e) {
+      throw new HttpResponseProcessingException(e);
+    }
   }
 
   public String getRecommend(String placeName){
