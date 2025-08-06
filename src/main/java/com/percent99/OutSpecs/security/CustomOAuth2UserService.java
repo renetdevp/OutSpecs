b4f193c1282459acf5f780c6d072c3278c9c5281
Hasegos <csw020106@naver.com>
2025-08-06 16:03:39 +0900

    FIX : CustomOAuth2UserService 내 javadoc 수정
    각 메서드별 javadoc 완성 및 name 필드 사용안함으로 삭제

--- src/main/java/com/percent99/OutSpecs/security/CustomOAuth2UserService.java
+++ src/main/java/com/percent99/OutSpecs/security/CustomOAuth2UserService.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 /**
- *
+ * OAuth2 로그인 사용자 정보를 관리하는 서비스
  */
 @Slf4j
 @Service
@@ -28,6 +28,12 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * OAuth2 인증 요청을 처리하고 사용자 정보를 로드한뒤 process 를 통해 CustomUserPrincipal로 반환한다.
+     * @param userRequest OAuth2 클라이언트 설정 및 액세스 토큰을 포함한 요청객체
+     * @return CustomUserPrincipal로 매핑된 OAuth2User 객체
+     * @throws OAuth2AuthenticationException 이메일 누락 등 처리 예외가 발생 시
+     */
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
             OAuth2User oAuth2User = super.loadUser(userRequest);
@@ -41,10 +47,15 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     }
 
     /**
-     *
-     * @param req
-     * @param ou
-     * @return
+     * OAuth2 프로바이더별로 내려온 사용자 속성을 기반으로 다음을 수행한다.
+     * <ul>
+     *   <li>기존 사용자(email 기준)가 providerId를 갖고 있으면 기존 OAuth2 사용자로 간주하고 즉시 로그인 처리</li>
+     *   <li>기존 사용자이지만 providerId가 없으면 폼 로그인 계정으로 간주하여 예외 발생</li>
+     *   <li>신규 사용자인 경우 안전한 랜덤 비밀번호를 생성하고 User 엔티티를 저장한 뒤 로그인 처리</li>
+     * </ul>
+     * @param req OAuth2UserRequest 객체
+     * @param ou DefaultOAuth2UserService가 로드한 원본 OAuth2User
+     * @return CustomUserPrincipal로 매핑된 OAuth2User
      */
     private OAuth2User process(OAuth2UserRequest req, OAuth2User ou){
         String regId = req.getClientRegistration().getRegistrationId();
@@ -60,28 +71,20 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                 ? (String) attributes.get("sub")
                 : (String) attributes.get("id");
 
-        String name = regId.equals("google")
-                ? (String) attributes.get("name")
-                : (String) attributes.get("login");
-
-
         Optional<User> userOptional = userRepository.findByUsername(email);
-        User user;
         if(userOptional.isPresent()){
-            user = userOptional.get();
+           User user = userOptional.get();
 
             if(user.getProviderId() != null && !user.getProviderId().isBlank()){
                 return new CustomUserPrincipal(user,attributes);
             }
-
             throw new OAuth2AuthenticationException(
                     "이미 폼 로그인으로 가입된 이메일입니다. 아이디/비밀번호로 로그인해주세요.");
 
         }else{
-
             String randomPassword = generateSecureRandomPassword();
 
-            user = new User();
+            User user = new User();
             user.setUsername(generateUniqueUsername(email));
             user.setProviderId(socialId);
             user.setRole(UserRoleType.USER);
@@ -102,9 +105,13 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     }
 
     /**
-     * 
-     * @param email
-     * @return
+     * 이메일 주소 앞부분을 기반으로 중복되지 않는 username을 생성한다. <br>
+     * 이미 같은 username이 존재할 경우 숫자를 붙여 충돌을 해소한다. <br><br>
+     *
+     * ex) <br>
+     * test@naver.com / test@company.com 이런경우
+     * @param email 사용자 이메일 주소
+     * @return 중복없는 username
      */
     private String generateUniqueUsername(String email){
         String base = email.substring(0, email.indexOf("@"));
