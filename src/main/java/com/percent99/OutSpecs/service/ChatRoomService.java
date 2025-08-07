6f3c72e5fad0cd26452f21ded82abc25fa1fc078
renetdevp <renetdevp@gmail.com>
2025-08-07 16:28:01 +0900

    FIX: Check user has a profile when create chatroom
    사용자가 채팅방을 생성할 때, 채팅을 하고자 하는 양측 유저가 모두 profile을 가지고 있는지 확인

--- src/main/java/com/percent99/OutSpecs/service/ChatRoomService.java
+++ src/main/java/com/percent99/OutSpecs/service/ChatRoomService.java
@@ -3,9 +3,11 @@ package com.percent99.OutSpecs.service;
 import com.percent99.OutSpecs.entity.ChatRoom;
 import com.percent99.OutSpecs.entity.User;
 import com.percent99.OutSpecs.repository.ChatRoomRepository;
+import com.percent99.OutSpecs.repository.ProfileRepository;
 import com.percent99.OutSpecs.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -17,12 +19,17 @@ import java.util.Optional;
 public class ChatRoomService {
   private final ChatRoomRepository chatRoomRepository;
   private final UserRepository userRepository;
+  private final ProfileRepository profileRepository;
 
+  @Transactional
   public ChatRoom createChatRoom(Long userId, Long targetId){
     User user1 = userRepository.findById(userId).orElse(null);
     User user2 = userRepository.findById(targetId).orElse(null);
 
     if (user1==null || user2==null) return null;
+
+    if (!profileRepository.existsByUserId(userId)) return null;
+    if (!profileRepository.existsByUserId(targetId)) return null;
 
     if (chatRoomRepository.existsByUser1AndUser2(user1, user2)) return null;
     if (chatRoomRepository.existsByUser1AndUser2(user2, user1)) return null;
@@ -39,12 +46,14 @@ public class ChatRoomService {
     return chatRoomRepository.save(chatRoom);
   }
 
+  @Transactional(readOnly = true)
   public Optional<ChatRoom> findChatRoomById(Long chatRoomId){
     if (chatRoomId == null) return Optional.empty();
 
     return chatRoomRepository.findById(chatRoomId);
   }
 
+  @Transactional
   public ChatRoom updateChatRoomById(ChatRoom chatRoom, Long userId){
     if (chatRoom==null || userId==null) return null;
     if (!chatRoomRepository.existsByIdAndUserId(chatRoom.getId(), userId)) return null;
@@ -52,6 +61,7 @@ public class ChatRoomService {
     return chatRoomRepository.save(chatRoom);
   }
 
+  @Transactional
   public void deleteChatRoomById(Long chatRoomId, Long userId){
     if (chatRoomId==null || userId==null) return;
     if (!chatRoomRepository.existsByIdAndUserId(chatRoomId, userId)) return;
