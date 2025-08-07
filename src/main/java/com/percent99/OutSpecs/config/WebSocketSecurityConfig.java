d35b7a4d664d041b79efbb5ab690457f1d08db71
renetdevp <renetdevp@gmail.com>
2025-08-07 10:04:47 +0900

    ADD: Add WebSocket Security Config
    Add WebSocket Security Config
    Authenticate required for Chatting

---
+++ src/main/java/com/percent99/OutSpecs/config/WebSocketSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.percent99.OutSpecs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+
+@Configuration
+@EnableWebSocketSecurity
+public class WebSocketSecurityConfig {
+  @Bean
+  AuthorizationManager<Message<?>> messageAuthorizationManager(MessageMatcherDelegatingAuthorizationManager.Builder messages){
+    messages
+            .simpDestMatchers("/app/chats/**").authenticated()
+            .simpSubscribeDestMatchers("/topic/rooms/**").authenticated()
+            .anyMessage().denyAll();
+
+    return messages.build();
+  }
+}
