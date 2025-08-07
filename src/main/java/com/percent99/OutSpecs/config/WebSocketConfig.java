2a902162144ec78d4ac0f8ae933cff3ece6c279c
renetdevp <renetdevp@gmail.com>
2025-08-07 10:01:59 +0900

    ADD: Add WebSocket Config
    Add WebSocket Config for Chatting

---
+++ src/main/java/com/percent99/OutSpecs/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.percent99.OutSpecs.config;
+
+import com.percent99.OutSpecs.interceptor.ChatInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+  private final ChatInterceptor chatInterceptor;
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/topic");
+    registry.setApplicationDestinationPrefixes("/app");
+  }
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws").withSockJS();
+  }
+
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(chatInterceptor);
+  }
+}
