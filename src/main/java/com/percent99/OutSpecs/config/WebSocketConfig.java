0e4d7d09ed433fe9cc99c8d93a095cc1196f0f72
renetdevp <renetdevp@gmail.com>
2025-08-10 23:20:18 +0900

    FIX: Replace simpleBroker route from '/topic' to '/queue', add StompEndpoint '/ws' without sockJS()
    simpleBroker의 경로를 /topic에서 /queue로 변경
    .sockJS() 체이닝이 없는 '/ws' StompEndpoint를 추가

--- src/main/java/com/percent99/OutSpecs/config/WebSocketConfig.java
+++ src/main/java/com/percent99/OutSpecs/config/WebSocketConfig.java
@@ -17,13 +17,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableSimpleBroker("/topic");
+    registry.enableSimpleBroker("/queue");
     registry.setApplicationDestinationPrefixes("/app");
   }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws").withSockJS();
+    registry.addEndpoint("/ws");
   }
 
   @Override
