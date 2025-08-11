be1dbf344fb0c3542240530c66aeedffa3fc2491
renetdevp <renetdevp@gmail.com>
2025-08-11 16:32:33 +0900

    FIX: Change field of ChatRoomResponseDTO from lastMessageId to lastMessage, Add lastMessageCreatedAt field of ChatRoomResponseDTO
    ChatRoomResponseDTO의 lastMessageId 필드를 lastMessage 필드로 변경
    ChatRoomResponseDTO에 lastMessageCreatedAt 필드를 추가

--- src/main/java/com/percent99/OutSpecs/dto/ChatRoomResponseDTO.java
+++ src/main/java/com/percent99/OutSpecs/dto/ChatRoomResponseDTO.java
@@ -3,12 +3,16 @@ package com.percent99.OutSpecs.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class ChatRoomResponseDTO {
   private Long chatRoomId;
   private boolean chatRoomIsChatBot;
-  private Long chatRoomLastMessageId;
+//  private Long chatRoomLastMessageId;
+  private String chatRoomLastMessage;
+  private LocalDateTime chatRoomLastMessageCreatedAt;
 
   private Long user1Id;
   private String user1Nickname;
