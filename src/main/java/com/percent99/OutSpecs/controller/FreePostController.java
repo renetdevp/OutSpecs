3120aa5603bb079a12e24742f2bbf2bf73386565
SuyunLee <dufrhdsy@naver.com>
2025-08-11 09:09:22 +0900

    FIX: 자유게시판 댓글 기능 추가
    자유게시판 댓글 기능 추가

--- src/main/java/com/percent99/OutSpecs/controller/FreePostController.java
+++ src/main/java/com/percent99/OutSpecs/controller/FreePostController.java
@@ -1,9 +1,12 @@
 package com.percent99.OutSpecs.controller;
 
+import com.percent99.OutSpecs.dto.CommentDTO;
 import com.percent99.OutSpecs.dto.PostDTO;
+import com.percent99.OutSpecs.entity.Comment;
 import com.percent99.OutSpecs.entity.Post;
 import com.percent99.OutSpecs.entity.PostType;
 import com.percent99.OutSpecs.security.CustomUserPrincipal;
+import com.percent99.OutSpecs.service.CommentService;
 import com.percent99.OutSpecs.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,12 +14,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/free-board")
 @RequiredArgsConstructor
 public class FreePostController {
 
     private final PostService postService;
+    private final CommentService commentService;
 
     @GetMapping
     public String freePostForm(Model model) {
@@ -52,7 +58,7 @@ public class FreePostController {
         if (postDTO == null) {
             return "redirect:/free-board/latest";
         }
-        model.addAttribute("postID", postId);
+        model.addAttribute("postId", postId);
         model.addAttribute("postDTO", postDTO);
         model.addAttribute("isEdit", true);
         return "post/free-board-write";
@@ -63,8 +69,8 @@ public class FreePostController {
                                  @PathVariable Long postId, @ModelAttribute PostDTO dto) {
         dto.setType(PostType.FREE);
         dto.setUserId(principal.getUser().getId());
-        postService.updatePost(postId, dto);
-        return "redirect:/free-board/" + postId;
+        Post post = postService.updatePost(postId, dto);
+        return "redirect:/free-board/" + post.getId();
     }
 
     @PostMapping("/{postId}/delete")
@@ -78,8 +84,37 @@ public class FreePostController {
     @GetMapping("/{postId}")
     public String detailFreePost(@PathVariable Long postId, Model model) {
         Post post = postService.getPostById(postId);
+        List<Comment> comments = commentService.getCommentsByPostId(postId);
         model.addAttribute("post", post);
+        model.addAttribute("comments", comments);
+        model.addAttribute("commentDTO", new CommentDTO());
         return "post/free-board-detail";
     }
 
+    @PostMapping("/{postId}/comment")
+    public String addCommentPost(@AuthenticationPrincipal CustomUserPrincipal principal,
+                                 @PathVariable Long postId, @ModelAttribute CommentDTO dto) {
+        dto.setUserId(principal.getUser().getId());
+        commentService.createComment(dto);
+        return "redirect:/free-board/" + postId;
+    }
+
+    @PostMapping("/{postId}/comment/{commentId}")
+    public String deleteCommentFreePost(@AuthenticationPrincipal CustomUserPrincipal principal,
+                                 @PathVariable Long postId,
+                                 @PathVariable Long commentId) {
+        Long userId = principal.getUser().getId();
+        commentService.deletedComment(userId, commentId);
+        return "redirect:/free-board/" + postId;
+    }
+
+    @PostMapping("/{postId}/comment/{commentId}/edit")
+    public String editCommentFreePost(@AuthenticationPrincipal CustomUserPrincipal principal,
+                                      @PathVariable Long postId,
+                                      @PathVariable Long commentId,
+                                      @ModelAttribute CommentDTO dto) {
+        dto.setUserId(principal.getUser().getId());
+        commentService.updateComment(commentId, dto);
+        return "redirect:/free-board/" + postId;
+    }
 }
