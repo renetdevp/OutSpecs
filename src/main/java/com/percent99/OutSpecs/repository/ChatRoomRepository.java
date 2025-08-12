adfa09079c6235dbbe2f59004e0dd2d5a6c2eb34
renetdevp <renetdevp@gmail.com>
2025-08-12 15:49:59 +0900

    FIX: Remove ChatRoomRepository.findByUsername and ChatRoomRepository.existsByUser1AndUser2, Add ChatRoomRepository.existsByUser1IdAndUser2Id and ChatRoomRepository.findAllByUserId
    ChatRoomRepository에 findByUsername()과 existsByUser1AndUser2() 메소드 삭제
    ChatRoomRepository에 existsByUser1IdAndUser2Id()와 findAllByUserId() 메소드 추가

--- src/main/java/com/percent99/OutSpecs/repository/ChatRoomRepository.java
+++ src/main/java/com/percent99/OutSpecs/repository/ChatRoomRepository.java
@@ -11,13 +11,14 @@ import java.util.List;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-  @Query(value = "SELECT cr FROM ChatRoom cr WHERE cr.user1.username = :username OR cr.user2.username = :username")
-  List<ChatRoom> findByUsername(@Param("username") String username);
-
-  boolean existsByUser1AndUser2(User user1, User user2);
+  @Query(value = "SELECT EXISTS (SELECT 1 FROM chat_rooms WHERE (user1_id = :user1Id AND user2_id = :user2Id) OR (user1_id = :user2Id AND user2_id = :user1Id))", nativeQuery = true)
+  boolean existsByUser1IdAndUser2Id(@Param("user1Id") Long user1Id, @Param("user2Id") Long user2Id);
 
   @Query(value = "SELECT EXISTS (SELECT 1 FROM chat_rooms WHERE id = :chatRoomId AND (user1_id = :userId OR user2_id = :userId))", nativeQuery = true)
   boolean existsByIdAndUserId(@Param("chatRoomId") Long chatRoomId, @Param("userId") Long userId);
+
+  @Query("SELECT cr FROM ChatRoom cr WHERE cr.user1.id = :userId OR cr.user2.id = :userId")
+  List<ChatRoom> findAllByUserId(@Param("userId") Long userId);
 
   @Query("DELETE FROM ChatRoom cr WHERE cr.user1.id = :userId OR cr.user2.id = :userId")
   void deleteAllByUserId(@Param("userId") Long userId);
