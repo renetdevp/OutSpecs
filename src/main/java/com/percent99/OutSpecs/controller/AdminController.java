52bbc32841622412a46c18a89d780e270c645832
Hasegos <csw020106@naver.com>
2025-08-13 11:38:30 +0900

    FIX : AdminController 회원 상태 변경 추가
    회원 상태 변경 추가 및 ADMIN일 경우만 작동

--- src/main/java/com/percent99/OutSpecs/controller/AdminController.java
+++ src/main/java/com/percent99/OutSpecs/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.percent99.OutSpecs.service.AdminService;
 import com.percent99.OutSpecs.service.PostService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,20 +20,20 @@ public class AdminController {
     private final AdminService adminService;
     private final PostService postService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public String showAdmin(@AuthenticationPrincipal CustomUserPrincipal principal,
                             Model model){
-        UserRoleType role = principal.getUser().getRole();
-        if(!role.name().equals( "ADMIN")){
-            return "redirect:/";
-        }
         model.addAttribute("users",adminService.findAllUsers());
         model.addAttribute("posts",adminService.findReportedPosts());
+        model.addAttribute("user",principal.getUser());
         return "admin/admin";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/users/{userId}/role")
-    public String changeUserRole(@PathVariable Long userId,
+    public String changeUserRole(@AuthenticationPrincipal CustomUserPrincipal principal,
+                                 @PathVariable Long userId,
                                  @RequestParam("role") UserRoleType role){
         try {
             adminService.changeUserRole(userId, role);
@@ -42,13 +43,51 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    @PostMapping("posts/{postId}/delete")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/posts/{postId}/delete")
     public String deletePost(@AuthenticationPrincipal CustomUserPrincipal principal,
                              @PathVariable Long postId){
         Long meId = principal.getUser().getId();
         try{
             postService.deletedPost(meId, postId);
         } catch (EntityNotFoundException e){
+            return "redirect:/admin?error=" + e.getMessage();
+        }
+        return "redirect:/admin";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/users/{userId}/ban")
+    public String banUserByAdmin(@PathVariable Long userId){
+
+        try {
+            adminService.banUser(userId);
+        }catch (Exception e){
+            return "redirect:/admin?error=" + e.getMessage();
+        }
+        return "redirect:/admin";
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/users/{userId}/unban")
+    public String unbanUserByAdmin(@PathVariable Long userId){
+
+        try {
+            adminService.unBanUser(userId);
+        }catch (Exception e){
+            return "redirect:/admin?error=" + e.getMessage();
+        }
+        return "redirect:/admin";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/users/{userId}/delete")
+    public String deleteUserByAdmin(@PathVariable Long userId){
+
+        try{
+            adminService.deleteUser(userId);
+        }catch (EntityNotFoundException e){
             return "redirect:/admin?error=" + e.getMessage();
         }
         return "redirect:/admin";
