6dd1a15dd83c5ef2fb4da495830e5408a50d608f
Hasegos <csw020106@naver.com>
2025-08-18 15:25:50 +0900

    FIX : PostListController 좋아요, 댓글수 추가
    좋아요, 댓글수 추가

--- src/main/java/com/percent99/OutSpecs/controller/PostListController.java
+++ src/main/java/com/percent99/OutSpecs/controller/PostListController.java
@@ -1,5 +1,6 @@
 package com.percent99.OutSpecs.controller;
 
+import com.percent99.OutSpecs.dto.PostListViewDTO;
 import com.percent99.OutSpecs.entity.*;
 import com.percent99.OutSpecs.security.CustomUserPrincipal;
 import com.percent99.OutSpecs.service.PostQueryService;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/list")
 @RequiredArgsConstructor
@@ -17,57 +20,88 @@ public class PostListController {
     private final PostQueryService postQueryService;
 
     @GetMapping("/free")
-    public String freePostlist(@AuthenticationPrincipal CustomUserPrincipal principal,
+    public String freePostList(@AuthenticationPrincipal CustomUserPrincipal principal,
                                Model model) {
         User user = principal.getUser();
+
+        List<PostListViewDTO> popular = postQueryService.toViews(
+                postQueryService.getViewCountPosts(PostType.FREE, 10), true,false);
+        List<PostListViewDTO> recent = postQueryService.toViews(
+                postQueryService.getRecentPosts(PostType.FREE, 20), true, false);
+
+
         model.addAttribute("user", user);
-        model.addAttribute("popularPosts", postQueryService.getViewCountPosts(PostType.FREE, 10));
-        model.addAttribute("recentPosts", postQueryService.getRecentPosts(PostType.FREE, 20));
+        model.addAttribute("popularPosts", popular);
+        model.addAttribute("recentPosts", recent);
         model.addAttribute("postType", "FREE");
         return "post/list";
     }
 
     @GetMapping("/qna")
-    public String qnaPostlist(@AuthenticationPrincipal CustomUserPrincipal principal,
+    public String qnaPostList(@AuthenticationPrincipal CustomUserPrincipal principal,
                                Model model) {
         User user = principal.getUser();
+
+        List<PostListViewDTO> popular = postQueryService.toViews(
+                postQueryService.getViewCountPosts(PostType.QNA, 10), true,false);
+        List<PostListViewDTO> recent = postQueryService.toViews(
+                postQueryService.getRecentPosts(PostType.QNA, 20), true, false);
+
         model.addAttribute("user", user);
-        model.addAttribute("popularPosts", postQueryService.getViewCountPosts(PostType.QNA, 10));
-        model.addAttribute("recentPosts", postQueryService.getRecentPosts(PostType.QNA, 20));
+        model.addAttribute("popularPosts",popular);
+        model.addAttribute("recentPosts", recent);
         model.addAttribute("postType", "QNA");
         return "post/list";
     }
 
     @GetMapping("/team")
-    public String teamPostlist(@AuthenticationPrincipal CustomUserPrincipal principal,
+    public String teamPostList(@AuthenticationPrincipal CustomUserPrincipal principal,
                               Model model, @ModelAttribute("errorMessage") String errorMessage) {
         User user = principal.getUser();
+
+        List<PostListViewDTO> popular = postQueryService.toViews(
+                postQueryService.getViewCountPosts(PostType.TEAM, 10), true,false);
+        List<PostListViewDTO> recent = postQueryService.toViews(
+                postQueryService.getRecentPosts(PostType.TEAM, 20), true, false);
+
         model.addAttribute("user", user);
-        model.addAttribute("popularPosts", postQueryService.getViewCountPosts(PostType.TEAM, 10));
-        model.addAttribute("recentPosts", postQueryService.getRecentPosts(PostType.TEAM, 20));
+        model.addAttribute("popularPosts", popular);
+        model.addAttribute("recentPosts", recent);
         model.addAttribute("postType", "TEAM");
         model.addAttribute("errorMessage", errorMessage);
         return "post/team-list";
     }
 
     @GetMapping("/recruit")
-    public String recruitPostlist(@AuthenticationPrincipal CustomUserPrincipal principal,
+    public String recruitPostList(@AuthenticationPrincipal CustomUserPrincipal principal,
                                Model model) {
         User user = principal.getUser();
+
+        List<PostListViewDTO> popular = postQueryService.toViews(
+                postQueryService.getViewCountPosts(PostType.RECRUIT, 10), true,false);
+        List<PostListViewDTO> recent = postQueryService.toViews(
+                postQueryService.getRecentPosts(PostType.RECRUIT, 20), true, false);
+
         model.addAttribute("user", user);
-        model.addAttribute("popularPosts", postQueryService.getViewCountPosts(PostType.RECRUIT, 10));
-        model.addAttribute("recentPosts", postQueryService.getRecentPosts(PostType.RECRUIT, 20));
+        model.addAttribute("popularPosts", popular);
+        model.addAttribute("recentPosts", recent);
         model.addAttribute("postType", "RECRUIT");
         return "post/list";
     }
 
     @GetMapping("/play")
-    public String playPostlist(@AuthenticationPrincipal CustomUserPrincipal principal,
+    public String playPostList(@AuthenticationPrincipal CustomUserPrincipal principal,
                                Model model) {
         User user = principal.getUser();
+
+        List<PostListViewDTO> popular = postQueryService.toViews(
+                postQueryService.getViewCountPosts(PostType.PLAY, 10), true,false);
+        List<PostListViewDTO> recent = postQueryService.toViews(
+                postQueryService.getRecentPosts(PostType.PLAY, 20), true, false);
+
         model.addAttribute("user", user);
-        model.addAttribute("popularPosts", postQueryService.getViewCountPosts(PostType.PLAY, 10));
-        model.addAttribute("recentPosts", postQueryService.getRecentPosts(PostType.PLAY, 20));
+        model.addAttribute("popularPosts", popular);
+        model.addAttribute("recentPosts", recent);
         model.addAttribute("postType", "PLAY");
         return "post/list";
     }
