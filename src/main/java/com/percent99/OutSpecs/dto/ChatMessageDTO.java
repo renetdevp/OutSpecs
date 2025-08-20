a0dc9e3913fe9e447da160d5e2f6e3b968a00a3b
renetdevp <renetdevp@gmail.com>
2025-08-20 22:22:42 +0900

    FIX: Add FullArgsConstructor to ChatMessageDTO
    ChatMessageDTO에 FullArgsConstructor 추가

--- src/main/java/com/percent99/OutSpecs/dto/ChatMessageDTO.java
+++ src/main/java/com/percent99/OutSpecs/dto/ChatMessageDTO.java
@@ -2,15 +2,24 @@ package com.percent99.OutSpecs.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ChatMessageDTO {
   private Long senderId;
   @NotBlank private String content;
   private LocalDateTime createdAt;
   private Long chatRoomId;
+
+  public ChatMessageDTO(Long senderId, String content, LocalDateTime createdAt, Long chatRoomId){
+    this.senderId = senderId;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.chatRoomId = chatRoomId;
+  }
 }
