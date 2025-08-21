466e948b73aba964f7d704d30cbfb8ce8cf3a551
renetdevp <renetdevp@gmail.com>
2025-08-21 10:40:40 +0900

    FIX: Describe UserRoleType enum value 'CHATBOT'
    UserRoleType의 CHATBOT 값에 대한 설명 추가

--- src/main/java/com/percent99/OutSpecs/entity/UserRoleType.java
+++ src/main/java/com/percent99/OutSpecs/entity/UserRoleType.java
@@ -5,7 +5,8 @@ package com.percent99.OutSpecs.entity;
  * 
  * ENTUSER : 기업회원 <br>
  * USER : 일반회원 <br>
- * ADMIN : 관리자
+ * ADMIN : 관리자 <br>
+ * CHATBOT : 챗봇
  */
 public enum UserRoleType {
     ENTUSER,
